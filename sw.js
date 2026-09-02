896eddbbbafdfb66dcd45bdfaa645e71c597e186
mxd-faahim <mdfaahim.444@gmail.com>
2026-09-03 01:09:35 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "timetable-v6";
+const CACHE = "timetable-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
